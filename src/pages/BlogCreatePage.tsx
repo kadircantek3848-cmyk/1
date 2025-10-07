fa97c8b059ecf2b95475f336f5d97fead0a4c435
kadircantek <127334932+kadircantek@users.noreply.github.com>
2025-10-07 22:16:38 +0300

    Create BlogCreatePage.tsx

---
+++ src/pages/BlogCreatePage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BlogCreatePage as BlogCreatePageComponent } from '../components/blog/BlogCreatePage';
+
+export function BlogCreatePage() {
+  return <BlogCreatePageComponent />;
+}
